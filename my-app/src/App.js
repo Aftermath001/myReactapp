af0f341c409a252ef8e41d4458b7591f35724ee2
Aftermath001 <alvinasiachi@gmail.com>
2023-04-17 22:40:20 +0300

    Sec Commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useCallback, useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0)
+   const handleClick = () =>  {
+    setCounter (counter +1);
+    alert ('You clicked this button');
+   }; 
+   
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +19,7 @@ function App() {
         It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.
         </p>
         <button onClick={() => alert('You clicked this button')}>Click Me!</button>
-
+        <p> You clicked this button {counter}</p>
         
         <a
           className="App-link"
